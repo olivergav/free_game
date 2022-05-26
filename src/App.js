d08aeb581c2bd3cdc8eb7806c7922834ec0ed2c9
olivergav <oliwier.gawlik0@gmail.com>
2022-05-26 18:50:05 +0200

    add constants, updated .gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/" element={<App />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/games/:id" element={<h1>siema</h1>} />
           </Routes>
       </BrowserRouter>
   );
